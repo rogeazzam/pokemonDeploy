3c2868199244620b021e77ea6d0b130aa133ad68
rogeazzam <roge_azzam@hotmail.com>
2024-06-02 22:32:12 +0300

    Modifyin the terraform file, and private user_data file

--- pokemon.tf
+++ pokemon.tf
@@ -176,7 +176,7 @@ resource "aws_instance" "public_ec2" {
   subnet_id     = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.publicSG.id]
 
-  user_data = file("/home/roge/Desktop/aws/terraforms/pokemonAPI/user_data_public.sh")
+  user_data = file("user_data_public.sh")
 
   tags = {
     Name = "public-ec2"
@@ -190,7 +190,7 @@ resource "aws_instance" "private_ec2" {
   subnet_id     = aws_subnet.private.id
   vpc_security_group_ids = [aws_security_group.privateSG.id]
 
-  user_data = file("/home/roge/Desktop/aws/terraforms/pokemonAPI/user_data_private.sh")
+  user_data = file("user_data_private.sh")
 
   tags = {
     Name = "private-ec2"
--- user_data_public.sh
+++ user_data_public.sh
@@ -4,6 +4,6 @@ sudo yum update -y
 sudo yum install -y docker
 sudo systemctl start docker
 sudo usermod -a -G docker ec2-user
-sudo docker pull rogeazzam/pokemon2
+sudo docker pull rogeazzam/pokemon-app
 sudo systemctl start nginx
 sudo systemctl enable nginx
